288ff95b7573c0bdd40cfb962067710203321338
Ravi Vaidya <RAVS@saileshs-MacBook-Pro.local>
2017-12-28 20:48:12 +0000

    Google maps api added onto about us page with relavent javascript, css and html code implemented

--- javascript/data.js
+++ javascript/data.js
@@ -1,7 +1,20 @@
-@keyframes layer2anim {
-  0%{
-    top: 5px;
-  }
-  50%{
-    top: 100px;
+"use strict";
+
+function initMap() {
+  let Cineworld = {lat: 52.772000, lng:-1.203900};
+  let mapDemo = document.getElementById("map");
+
+
+  let map = new google.maps.Map(mapDemo, {
+      zoom: 16,
+      center: Cineworld
+    });
+
+  // these set different markers you want to show on your map
+  let markerCineworld = new google.maps.Marker({
+    position: Cineworld,
+    map: map,
+    title: 'Cineworld'
+
+  });
 }
